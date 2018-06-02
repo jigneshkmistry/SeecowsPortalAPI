6fcffb4ca3263627d4b73821d552dc128e2dc601
jigneshkmistry <jignesh@trivediat.com>
2018-06-02 19:47:25 +0530

    cmt 3

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,22 +1,35 @@
 const express = require('express');
-
-const books = [
-    { id: 1, title: "War and Peace", genre: "Historical Fiction", author: "Jignesh Mistry", read: false },
-    { id: 2, title: "The Time Machine", genre: "Science Fiction", author: "Kishan Solanki", read: false }
-  ];
+const sql = require('mssql');
+const debug = require('debug')('app:bookRoutes')
 
 const bookRouter = express.Router();
-  
+
 bookRouter.route('/')
-.get((req, res) => {
-  res.render("books", { books });
-});
+  .get((req, res) => {
+    const request = new sql.Request();
+    request.query('select * from Book').then(result => {
+      res.render("bookListView", { books : result.recordset });
+    });
+  });
 
 bookRouter.route('/:id')
-.get((req, res) => {
-    const bookID = req.params.id;
-  res.render("book",books[bookID]);
-});
+  .all((req, res, next) => {
+    getBookByID(req, next);
+  })
+  .get((req, res) => {
+    res.render("bookView", { book: req.book });
+  });
+  
+function getBookByID(req, next) {
+  const bookID = req.params.id;
+  const request = new sql.Request();
+  request.input('id', sql.Int, bookID)
+    .query('select * from Book where id = @id')
+    .then(result => {
+      req.book = result.recordset[0];
+      next();
+    });
+}
 
 module.exports = bookRouter;
 
